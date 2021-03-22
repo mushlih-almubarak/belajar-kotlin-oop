263674037a9ab7250418128af053c897bb41f081
Mushlih Almubarak <mushlih2004@gmail.com>
2021-03-22 17:00:10 +0300

    Memodifikasi dan mengubah sebagian kode

---
+++ src/App/Constructor.kt
@@ -0,0 +1,16 @@
+package App
+
+import Data.Mobil
+/*Saat kita membuat Object, maka kita seperti memanggil sebuah function
+Di dalam class Kotlin, kita bisa membuat Constructors, Constructors mirip seperti function yang akan dipanggil saat pertama kali Object dibuat.
+Mirip seperti di Function, kita bisa memberi parameter pada Constructors
+ */
+fun main() {
+    val mobilToyota = Mobil("Toyota")
+    var mobilAvanza = Mobil("Avanza", 2019)
+
+    println(mobilToyota.merek)
+    println(mobilToyota.tahun)
+    println(mobilAvanza.merek)
+    println(mobilAvanza.tahun)
+}
---
+++ src/Data/Mobil.kt
@@ -0,0 +1,6 @@
+package Data
+
+class Mobil(parameterMerek:String, parameterTahun:Int = 2020) {
+    var merek = parameterMerek
+    var tahun = parameterTahun
+}
