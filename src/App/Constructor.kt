5d452ea501ead8db639dcdb8d4d78faecc5d0eac
Mushlih Almubarak <mushlih2004@gmail.com>
2021-03-22 17:02:02 +0300

    Menambahkan tanda petik

--- src/App/Constructor.kt
+++ src/App/Constructor.kt
@@ -2,7 +2,7 @@ package App
 
 import Data.Mobil
 /*Saat kita membuat Object, maka kita seperti memanggil sebuah function
-Di dalam class Kotlin, kita bisa membuat Constructors, Constructors mirip seperti function yang akan dipanggil saat pertama kali Object dibuat.
+Di dalam class Kotlin, kita bisa membuat "Constructors", Constructors mirip seperti function yang akan dipanggil saat pertama kali Object dibuat.
 Mirip seperti di Function, kita bisa memberi parameter pada Constructors
  */
 fun main() {
